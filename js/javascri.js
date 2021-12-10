ff45c9ece081ffc1ac7ac0d5c890d4f5ff6a0404
Simra <72741150+simra123@users.noreply.github.com>
2021-12-10 14:08:01 +0500

    added

---
+++ js/javascri.js
@@ -0,0 +1,24 @@
+
+// portfolio images popups
+
+function img1(){
+    window.open("file:///E:/Ayesha%20Zia/images/col11.jpeg" , "bride in white")
+}
+
+function img2(){
+    window.open("file:///E:/Ayesha%20Zia/images/col12.jpeg")
+}
+
+function img3(){
+    window.open("file:///E:/Ayesha%20Zia/images/col13.jpeg")
+}
+
+function img4(){
+    window.open("file:///E:/Ayesha%20Zia/images/col14.jpeg")
+}
+
+function img5(){
+    window.open("file:///E:/Ayesha%20Zia/images/col15.jpeg")
+}
+  var b = document.getElementsByTagName("a")
+  console.log(b)
